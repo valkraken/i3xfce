eaa4feb3e565df64932b3244b084a15c14315f3a
val <35927546+valkraken@users.noreply.github.com>
2021-03-12 17:36:22 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,8 +5,15 @@ sudo apt update && sudo apt upgrade -y
 sudo apt install i3-gaps nitrogen compton
 
 # adds i3-gaps, nitrogen and comton to autostart
-exo-desktop-item-edit --create-new --type Application --name i3 --command i3 ~/.config/autostart/
-exo-desktop-item-edit --create-new --type Application --name nitrogen --command "nitrogen --restore" ~/.config/autostart/
-exo-desktop-item-edit --create-new --type Application --name compton --command compton ~/.config/autostart/
+mkdir ~/.config/autostart
+echo "W0Rlc2t0b3AgRW50cnldCkVuY29kaW5nPVVURi04ClZlcnNpb249MC45LjQKVHlwZT1BcHBsaWNhdGlvbgpOYW1lPWkzCkNvbW1lbnQ9CkV4ZWM9aTMKT25seVNob3dJbj1YRkNFOwpSdW5Ib29rPTAKU3RhcnR1cE5vdGlmeT1mYWxzZQpUZXJtaW5hbD1mYWxzZQpIaWRkZW49ZmFsc2UKCiAg" | base64 --decode > i3.desktop
+echo "W0Rlc2t0b3AgRW50cnldCkVuY29kaW5nPVVURi04ClZlcnNpb249MC45LjQKVHlwZT1BcHBsaWNhdGlvbgpOYW1lPWNvbXB0b24KQ29tbWVudD0KRXhlYz1jb21wdG9uCk9ubHlTaG93SW49WEZDRTsKUnVuSG9vaz0wClN0YXJ0dXBOb3RpZnk9ZmFsc2UKVGVybWluYWw9ZmFsc2UKSGlkZGVuPWZhbHNlCgogICAg" | base64 --decode > compton.desktop
+echo "W0Rlc2t0b3AgRW50cnldCkVuY29kaW5nPVVURi04ClZlcnNpb249MC45LjQKVHlwZT1BcHBsaWNhdGlvbgpOYW1lPW5pdHJvZ2VuCkNvbW1lbnQ9CkV4ZWM9bml0cm9nZW4gLS1yZXN0b3JlCk9ubHlTaG93SW49WEZDRTsKUnVuSG9vaz0wClN0YXJ0dXBOb3RpZnk9ZmFsc2UKVGVybWluYWw9ZmFsc2UKSGlkZGVuPWZhbHNlCgogICAgICAg" | base64 --decode > nitrogen.desktop
+
+dbus-send --session --dest=org.xfce.SessionManager --print-reply \/org/xfce/SessionManager org.xfce.Session.Manager.Checkpoint string:""
+
+mv ~/.cache/sessions/xfce4-session-$(hostname):0 ~/.cache/sessions/xfce4-session-$(hostname):0.backup
+cat ~/.cache/sessions/xfce4-session-$(hostname):0.backup | grep -v $(grep Program=xfdesktop ~/.cache/sessions/xfce4-session-kali:0.backup | cut -d '_' -f 1) | grep -v $(grep Program=xfwm4 ~/.cache/sessions/xfce4-session-kali:0.backup | cut -d '_' -f 1) > ~/.cache/sessions/xfce4-session-kali:0
+
 
 echo "CgppMy94ZmNlIGh5YnJpZCBjb25maWd1cmF0aW9uIGNvbXBsZXRlISAKTG9nIG91dCBhbmQgYmFjayBpbiB0byBzdGFydCBhIHNlc3Npb24gaW4geW91ciBuZXcgZW52aW9ybWVudC4KVXNlIG5pdHJvZ2VuIHRvIHNldCB5b3VyIHByZWZlcmVkIHdhbGxwYXBlci4KSG9wZSB5b3UgZW5qb3khCgpCZXN0IHJlZ2FyZHMsCnZhbCA8Mwo=" | base64 --decode
